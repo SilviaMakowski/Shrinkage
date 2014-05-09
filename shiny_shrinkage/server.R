9d3463137966f582e0d58d257e12296499799aa0
Silvia Makowski <smakowsk@uni-potsdam.de>
2014-05-09 12:59:41 +0200

    shiny cleanup, generates spatial and attraction effect plot

--- shiny_shrinkage/server.R
+++ shiny_shrinkage/server.R
@@ -60,11 +60,45 @@ shinyServer(function(input, output) {
     dat$c2 <- ifelse(dat$tar=="val" | dat$tar=="sod", -0.5, +0.5)
     dat$c3 <- ifelse(dat$tar=="dod", -0.75, 0.25)
     
-    #First plot
-    # Transformations
-    # ... determine lambda (i.e., power coefficient); boxcox() is from MASS
-    # ... ... for exact lambda
-    lambdaList <- boxcox(rt ~ id*tar, data=dat)
+    ###########################
+    ### Linear Mixed Models ###
+    ###########################
+    
+    #### DV: rt
+    # ... fully parameterized model; add correlation parameters (see Tables 1 and 2, top left)
+    print(m2 <- lmer(rt ~ 1 + c1 + c2 + c3 + (1 + c1 + c2 + c3 | id), data=dat))  
+    
+    
+    # ... splom of coefficiencts (i.e., fixed effects plus conditional modes)
+    m2.coef <- unlist(coef(m2))
+    dim(m2.coef) <- c(61, 4)
+    m2.coef <- data.frame(m2.coef)
+    names(m2.coef) <- c("Mean", "Spatial", "Object", "Attraction")
+    
+    ###########################################################################
+    ### Figure 3: Compare conditional modes and within-subject OLS effects  ###
+    ###########################################################################
+    
+    # Within-subject OLS estimates
+    df <- coef(lmList(rt ~ tar | id, data=dat))
+    
+    m2.coef$id <- factor(1:61)
+    
+    df <- cbind(df, m2.coef)
+    
+    # Spatial and attraction effects
+    px1 <- with(df,
+                print(xyplot(tar.spt ~ tar.att, aspect = 1,
+                             x1 = Attraction, y1 = Spatial, xlab="Attraction Effect", ylab="Spatial Effect",
+                             panel = function(x, y, x1, y1, subscripts, ...) {
+                               panel.grid(h = -1, v = -1)
+                               x1 <- x1[subscripts]
+                               y1 <- y1[subscripts]
+                               panel.arrows(x, y, x1, y1, type = "open", length = 0.1, col="gray50", lty=1, angle = 15, ...)
+                               panel.points(x, y, col="black", cex=0.9, pch=1)
+                               panel.points(x1, y1, col="black", cex=0.7, pch=19)
+                             }
+                ))     )
     
   })
   
