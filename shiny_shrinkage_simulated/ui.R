ad1da7b41b938a1d211fb2a1b3460e6bf98c257e
adietz <dietz-alexander@gmx.de>
2014-06-25 13:46:55 +0200

    descriptions for sliders

--- shiny_shrinkage_simulated/ui.R
+++ shiny_shrinkage_simulated/ui.R
@@ -11,7 +11,7 @@ shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel(HTML("<h3>Shrinkage effects</h3> 
-              <p>This application can be used for illustrating shrinkage of observed data toward the estimated population mean in linear mixed models. LMM analyses are based on simulated data using mixedDesign() function (Hohenstein & Kliegl, 2013). Means, standard deviations and correlations used in mixedDesign() are calculated from experimental data by Kliegl, Wei, Dambacher, Yan and Zhou (2010) using the two-rectangle cueing paradigm (Egly, Driver & Rafal, 1994). </p>"), 
+              <p>This application can be used for illustrating shrinkage of observed data toward the estimated population mean in linear mixed models. LMM analyses are based on simulated data using mixedDesign() function (Hohenstein & Kliegl, 2013). Means, standard deviations and correlations used in mixedDesign() are calculated from experimental data by Kliegl, Wei, Dambacher, Yan and Zhou (2011) using the two-rectangle cueing paradigm (Egly, Driver & Rafal, 1994). </p>"), 
               windowTitle="Shiny Shrinkage"),
   
   # Sidebar with slider input
